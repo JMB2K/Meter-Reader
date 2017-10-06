72d18199e99a15c29b1f85829a106c2310d99c4d
JMB2K <jonathanmb2000@outlook.com>
2017-10-06 03:22:22 -0500

    added parse_it function

--- functions.py
+++ functions.py
@@ -19,32 +19,31 @@ def separator(city):
             file.write('\n\n' + '*' * 17 + '\n' + '*' * stars + city + '*' * stars + '\n' + '*' * 17 + '\n')
     file.close()
 
+def parse_it(s, url, params): # navigates to and collects the meter data
+    s.get(url + '/login', params=params)
+    s.get(url + 'sysmonitor', params=params)
+    s.get(url + '/rps/jstatpri.cgi', params=params)
+    counters = s.get(url + '/rps/dcounter.cgi', params=params)
+    soup = bs(counters.conent, 'html5lib')  # parsing from full page to just the meters
+    table = soup.table
+    script = table.find_all('script')
+    return script
 
 def canon(url, copier, specific_meters):
     s=requests.Session()
     meter = []
     meters = []
-    pins = ['DAL-MFP-K', 'NYC-MFP-B', 'DAL-MFP-H']
-    no_argos = ['DAL-MFP-J', 'DAL-MFP-M', 'LAX-JOBSI']
+    pins = ['DAL-MFP-K', 'NYC-MFP-B', 'DAL-MFP-H']. # argos onboard installed but uses pin instead of password in html
+    no_argos = ['DAL-MFP-J', 'DAL-MFP-M', 'LAX-JOBSI']  # no argos installed, uses the canon UI
 
     if copier[:9] in pins:
         params={'pin': '00015', 'originally-requested-url': '/rps/'}
-        s.get(url + '/login', params=params)
-        s.get(url + '/sysmonitor', params=params)  # have to go through all of these url for it to work
-        s.get(url + '/rps/jstatpri.cgi', params=params)
-        counters = s.get(url + '/rps/dcounter.cgi', params=params)
-        soup = bs(counters.content, 'html5lib')
-        table = soup.table
-        script = table.find_all('script')
+        script = parse_it(s, url, params)
+
     elif copier[:9] in no_argos:
         params={'deptid': '8888', 'loginType': 'admin', 'password': '', 'uri': '/', 'user_type_generic': ''}
-        s.get(url, params=params)
-        s.get(url + '/sysmonitor', params=params)  # have to go through all of these url for it to work
-        s.get(url + '/rps/jstatpri.cgi', params=params)
-        counters = s.get(url + '/rps/dcounter.cgi', params=params)
-        soup = bs(counters.content, 'html5lib')
-        table = soup.table
-        script = table.find_all('script')
+        script = parse_it(s, url, params)
+
     elif 'PHX-MFP-B' in copier:
         params = {'password': '00015', 'uri': '/rps/'}
         s.get(url, params=params)
@@ -56,13 +55,7 @@ def canon(url, copier, specific_meters):
         script = table.find_all('script')
     else:
         params={'password': '00015', 'uri': '/rps/'}
-        s.get(url, params=params)
-        s.get(url + '/sysmonitor', params=params)  # have to go through all of these url for it to work
-        s.get(url + '/rps/jstatpri.cgi', params=params)
-        counters = s.get(url + '/rps/dcounter.cgi', params=params)
-        soup = bs(counters.content, 'html5lib')
-        table = soup.table
-        script = table.find_all('script')
+        script = parse_it(s, url, params)
 
     for tag in script:  # Everything here and below is just parsing out the data from html/java
       tag = tag.text
@@ -74,7 +67,7 @@ def canon(url, copier, specific_meters):
         b=i.index(')')
         i=i[a:b+1]
         meter[count]=i
-        count=count+1
+        count+=1
     for i in meter:
       if i[2:5] in '105109124125108':
         i=i.replace('"', '')
@@ -83,8 +76,8 @@ def canon(url, copier, specific_meters):
     for i in range(len(meters)):
       a=[meters[count][1:4], meters[count][5:-1]]
       meters[count]=a
-      count=count+1
-    with open('C:\\Users\\00015\\Desktop\\meter_readings.txt', 'a') as f:
+      count+1
+    with open('C:\\Users\\00015\\Desktop\\meter_readings.txt', 'a') as f:  # writing meters to file
       f.write('\n' + copier + '\n')
       for i in meters:
         if i[0] in specific_meters:
@@ -98,7 +91,7 @@ def xerox(url, copier):
     binfo = my_script.split('var billInfo = ')
     binfo2 = str(binfo[1]).rsplit(';')      # parse the meter readings from java
     result = binfo2[0].replace('[', '').replace(']', '').replace(';', '').replace('\'', '').strip().split(',')
-    if 'MFP-T' in copier:
+    if 'MFP-T' in copier:  # organizing the data so it writes to file how I want it to
         a = result[:2]
         b = result[2:4]
         c = result[6:8]
@@ -109,7 +102,7 @@ def xerox(url, copier):
         c = result[9:11]
         d = result[12:14]
     results = [a, b, c, d]
-    with open('C:\\Users\\00015\\Desktop\\meter_readings.txt', 'a') as file:
+    with open('C:\\Users\\00015\\Desktop\\meter_readings.txt', 'a') as file:  # writing data to file
         file.write('\n' + copier + '\n')
         for item in results:
             file.write(str(item[0]) + ': ' + str(item[1]) + '\n')
