29d88fe8baa09ef8fff40cc69f6ac57095ccf4d1
JMB2K <jonathanmb2000@outlook.com>
2018-01-22 18:59:59 -0600

    cleaned up a lot, 30 seconds faster now

--- main.py
+++ main.py
@@ -3,77 +3,58 @@
 import requests, time, re
 from printers import printer_dict
 
-def parse_it(s, copier): # navigates to and collects the meter data
+def get_meters(copier): # navigates to and collects the meter data
     meters = []
-    url, params = copier['url'], copier['params']
-    s.get(url + '/login', params=params)
-    s.get(url + '/sysmonitor', params=params)
-    s.get(url + '/rps/jstatpri.cgi', params=params)
-    counters = s.get(url + '/rps/dcounter.cgi', params=params)
-    reads = re.findall('write_value\((\"[0-9]+\",[0-9]+)', counters.text)
-    for i in reads:
-        i=i.replace('"', '').split(',')
-        if i[0] in '109124105125108':
-            meters.append(i)
-    return meters
+    if copier['brand'] == 'canon':
+        s=requests.Session()
 
-def canon(copier):
-    s, url, params=requests.Session(), copier['url'], copier['params']
+        for site in copier['login_path']:
+            counters = s.get(copier['url'] + site, params=copier['params'])
 
-    if copier == printer_dict['PHX-MFP-B']:
-        meters = []
-        s.get(url, params=params)
-        s.get(url + '/rps/dstatus.cgi', params=params)  # have to go through all of these url for it to work
-        counters = s.get(url + '/rps/dcounter.cgi', params=params)
         reads = re.findall('write_value\((\"[0-9]+\",[0-9]+)', counters.text)
         for i in reads:
             i=i.replace('"', '').split(',')
             if i[0] in '109124105125108':
                 meters.append(i)
-    else:
-         meters = parse_it(s, copier)
+    elif copier['brand'] == 'xerox':
+        r = requests.get(copier['url'])  # get info and make soup
+        data = re.findall('billInfo = \[(.+)\]', r.text)
+        data = data[0].split(',')
+        while len(data) > 0:
+            temp = []
+            if copier == printer_dict['DAL-MFP-E']:
+                data.pop()
+            temp.append(data.pop().replace('\'', ''))
+            temp.append(data.pop().replace('\'', ''))
+            meters.append(temp[::-1])
+    return meters
 
+def write_to_file(copier, meters):
     with open('C:\\Users\\00015\\Desktop\\meter_readings.txt', 'a') as f:  # writing meters to file
         f.write('\n{}\n'.format(copier['label']))
-        for i in meters:
-            if i[0] in copier['specific_meters']:
-                f.write('{}: {}\n'.format(i[0], i[1]))
+        if copier['brand'] == 'canon':
+            for item in meters:
+                if item[0] in copier['specific_meters']:
+                    f.write('{}: {}\n'.format(item[0], item[1]))
+        elif copier['brand'] == 'xerox':
+            for item in meters:
+                if 'Total Impressions' not in item[0]:
+                    f.write('{}: {}\n'.format(item[0], item[1]))
         f.close()
-
-def xerox(copier):
-    r = requests.get(copier['url'])  # get info and make soup
-    meter_s = re.findall('billInfo = \[(.+)\]', r.text)
-    meter_s = meter_s[0].split(',')
-    results = []
-    while len(meter_s) > 0:
-        temp = []
-        if copier == printer_dict['DAL-MFP-E']:
-            meter_s.pop()
-        temp.append(meter_s.pop().replace('\'', ''))
-        temp.append(meter_s.pop().replace('\'', ''))
-        results.append(temp[::-1])
-
-    with open('C:\\Users\\00015\\Desktop\\meter_readings.txt', 'a') as file:  # writing data to file
-        file.write('\n{}\n'.format(copier['label']))
-        for item in results:
-            if 'Total Impressions' not in item[0]:
-                file.write('{}: {}\n'.format(item[0], item[1]))
-        file.close()
 
 
 if __name__ == '__main__':
     start = time.time()
     for item in printer_dict:
-        if printer_dict[item]['brand'] == 'canon':
-            try:
-                canon(printer_dict[item])
-            except Exception:
-                with open('C:\\Users\\00015\\Desktop\\meter_readings.txt', 'a') as f:  # writing meters to file
-                    f.write('\n' + printer_dict[item]['label'] + '\n')
-                    f.write('ERROR: NO METERS TAKEN\n')
-                    f.close()
-        elif printer_dict[item]['brand'] == 'xerox':
-            xerox(printer_dict[item])
+        try:
+            meters = get_meters(printer_dict[item])
+            write_to_file(printer_dict[item], meters)
+        except Exception:
+            with open('C:\\Users\\00015\\Desktop\\meter_readings.txt', 'a') as f:  # writing meters to file
+                f.write('\n' + printer_dict[item]['label'] + '\n')
+                f.write('ERROR: NO METERS TAKEN\n')
+                f.close()
+
         print('Finished with {}'.format(item))
     end = time.time() - start
 
--- printers.py
+++ printers.py
@@ -6,22 +6,26 @@ printer_dict['DAL-MFP-A'] =  {'brand': 'canon',
                'label': 'DAL-MFP-A ---> 55258',
                'url': 'http://10.21.20.104:8000',
                'params': {'password': '00015', 'uri': '/rps/'},
-               'specific_meters': '125108'}
+               'specific_meters': '125108',
+               'login_path': ['/login', '/sysmonitor', '/rps/jstatpri.cgi', '/rps/dcounter.cgi']}
 printer_dict['DAL-MFP-B'] = {'brand': 'canon',
                'label': 'DAL-MFP-B ---> 71156',
                'url': 'http://10.21.20.112:8000',
                'params': {'password': '00015', 'uri': '/rps/'},
-               'specific_meters': '125108'}
+               'specific_meters': '125108',
+               'login_path': ['/login', '/sysmonitor', '/rps/jstatpri.cgi', '/rps/dcounter.cgi']}
 printer_dict['DAL-MFP-C'] = {'brand': 'canon',
                'label': 'DAL-MFP-C ---> 55021',
                'url': 'http://10.21.20.108:8000',
                'params': {'password': '00015', 'uri': '/rps/'},
-               'specific_meters': '125108'}
+               'specific_meters': '125108',
+               'login_path': ['/login', '/sysmonitor', '/rps/jstatpri.cgi', '/rps/dcounter.cgi']}
 printer_dict['DAL-MFP-D'] = {'brand': 'canon',
                'label': 'DAL-MFP-D ---> 55259',
                'url': 'http://10.21.20.109:8000',
                'params': {'password': '00015', 'uri': '/rps/'},
-               'specific_meters': '125108'}
+               'specific_meters': '125108',
+               'login_path': ['/login', '/sysmonitor', '/rps/jstatpri.cgi', '/rps/dcounter.cgi']}
 printer_dict['DAL-MFP-E'] = {'brand': 'xerox',
                'label': 'DAL-MFP-E ---> 43644',
                'url': 'http://10.21.20.120:8080/prbillinfo.htm'}
@@ -29,39 +33,46 @@ printer_dict['DAL-MFP-F'] = {'brand': 'canon',
                'label': 'DAL-MFP-F ---> 55260',
                'url': 'http://10.21.20.105:8000',
                'params': {'password': '00015', 'uri': '/rps/'},
-               'specific_meters': '125108'}
+               'specific_meters': '125108',
+               'login_path': ['/login', '/sysmonitor', '/rps/jstatpri.cgi', '/rps/dcounter.cgi']}
 printer_dict['DAL-MFP-G'] = {'brand': 'canon',
                'label': 'DAL-MFP-G ---> 55215',
                'url': 'http://10.21.20.101:8000',
                'params': {'password': '00015', 'uri': '/rps/'},
-               'specific_meters': '125108'}
+               'specific_meters': '125108',
+               'login_path': ['/login', '/sysmonitor', '/rps/jstatpri.cgi', '/rps/dcounter.cgi']}
 printer_dict['DAL-MFP-H'] = {'brand': 'canon',
                'label': 'DAL-MFP-H ---> 55261',
                'url': 'http://10.21.20.106:8000',
                'params': {'pin': '00015', 'originally-requested-url': '/rps/'},
-               'specific_meters': '125108'}
+               'specific_meters': '125108',
+               'login_path': ['/login', '/sysmonitor', '/rps/jstatpri.cgi', '/rps/dcounter.cgi']}
 printer_dict['DAL-MFP-I'] = {'brand': 'canon',
                'label': 'DAL-MFP-I ---> 55262',
                'url': 'http://10.21.20.110:8000',
                'params': {'password': '00015', 'uri': '/rps/'},
-               'specific_meters': '108'}
+               'specific_meters': '108',
+               'login_path': ['/login', '/sysmonitor', '/rps/jstatpri.cgi', '/rps/dcounter.cgi']}
 printer_dict['DAL-MFP-J'] = {'brand': 'canon',
                'label': 'DAL-MFP-J ---> 39632',
                'url': 'http://10.21.20.102:8000',
                'params': {'deptid': '8888', 'loginType': 'admin', 'password': '',
                           'uri': '/', 'user_type_generic': ''},
-               'specific_meters': '125108'}
+               'specific_meters': '125108',
+               'login_path': ['/login', '/sysmonitor', '/rps/jstatpri.cgi', '/rps/dcounter.cgi']}
 printer_dict['DAL-MFP-K'] = {'brand': 'canon',
                'label': 'DAL-MFP-K ---> 55253',
                'url': 'http://10.21.20.103:8000',
                'params': {'pin': '00015', 'originally-requested-url': '/rps/'},
-               'specific_meters': '125108'}
+               'specific_meters': '125108',
+               'login_path': ['/login', '/sysmonitor', '/rps/jstatpri.cgi', '/rps/dcounter.cgi']}
 printer_dict['DAL-MFP-M'] = {'brand': 'canon',
                'label': 'DAL-MFP-M ---> 39631',
                'url': 'http://10.21.20.107:8000',
                'params': {'deptid': '8888', 'loginType': 'admin', 'password': '',
                           'uri': '/', 'user_type_generic': ''},
-               'specific_meters': '125108'}
+               'specific_meters': '125108',
+               'login_path': ['/login', '/sysmonitor', '/rps/jstatpri.cgi', '/rps/dcounter.cgi']}
 printer_dict['DAL-MFP-T'] = {'brand': 'xerox',
                'label': 'DAL-MFP-T ---> 23579',
                'url': 'http://10.21.150.103:8080/prbillinfo.htm'}
@@ -69,35 +80,42 @@ printer_dict['FRI-MFP-A'] = {'brand': 'canon',
                'label': 'FRI-MFP-A',
                'url': 'http://10.35.20.101:8000',
                'params': {'password': '00015', 'uri': '/rps/'},
-               'specific_meters': '125108'}
+               'specific_meters': '125108',
+               'login_path': ['/login', '/sysmonitor', '/rps/jstatpri.cgi', '/rps/dcounter.cgi']}
 printer_dict['HOU-MFP-A'] = {'brand': 'canon',
                'label': 'HOU-MFP-A',
                'url': 'http://10.37.20.101:8000',
                'params': {'password': '00015', 'uri': '/rps/'},
-               'specific_meters': '125108'}
+               'specific_meters': '125108',
+               'login_path': ['/login', '/sysmonitor', '/rps/jstatpri.cgi', '/rps/dcounter.cgi']}
 printer_dict['LAX-MFP-A'] = {'brand': 'canon',
                'label': 'LAX-MFP-A',
                'url': 'http://10.43.20.101:8000',
                'params': {'password': '00015', 'uri': '/rps/'},
-               'specific_meters': '124109'}
+               'specific_meters': '124109',
+               'login_path': ['/login', '/sysmonitor', '/rps/jstatpri.cgi', '/rps/dcounter.cgi']}
 printer_dict['NYC-MFP-A'] = {'brand': 'canon',
                'label': 'NYC-MFP-A',
                'url': 'http://10.33.20.101:8000',
                'params': {'password': '00015', 'uri': '/rps/'},
-               'specific_meters': '105108'}
+               'specific_meters': '105108',
+               'login_path': ['/login', '/sysmonitor', '/rps/jstatpri.cgi', '/rps/dcounter.cgi']}
 printer_dict['NYC-MFP-B'] = {'brand': 'canon',
                'label': 'NYC-MFP-B',
                'url': 'http://10.33.20.102:8000',
                'params': {'pin': '00015', 'originally-requested-url': '/rps/'},
-               'specific_meters': '105108'}
+               'specific_meters': '105108',
+               'login_path': ['/login', '/sysmonitor', '/rps/jstatpri.cgi', '/rps/dcounter.cgi']}
 printer_dict['PHX-MFP-A'] = {'brand': 'canon',
                'label': 'PHX-MFP-A',
                'url': 'http://10.31.20.102:8000',
                'params': {'password': '00015', 'uri': '/rps/'},
-               'specific_meters': '124109'}
+               'specific_meters': '124109',
+               'login_path': ['/login', '/sysmonitor', '/rps/jstatpri.cgi', '/rps/dcounter.cgi']}
 printer_dict['PHX-MFP-B'] = {'brand': 'canon',
                'label': 'PHX-MFP-B',
                'url': 'http://10.31.20.101:8000',
                'params': {'password': '00015', 'uri': '/rps/'},
-               'specific_meters': '125108'}
+               'specific_meters': '125108',
+               'login_path': ['/rps/dstatus.cgi', '/rps/dcounter.cgi']}
 
